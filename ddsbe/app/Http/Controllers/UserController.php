47d512878fd853f282c36bafc7874734ba5da73c
unknown <chembeeregaton5@gmail.com>
2024-03-02 16:39:51 +0800

    committed

---
+++ ddsbe/app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+namespace App\Models\User;
+
+use Illuminate\Http\Controllers;
+use App\User;
+
+Class UserController extends Controller 
+{
+    private $request;
+
+    public function __construct(Request $request)
+    {
+         $this->request = $request;
+    }
+
+    public function getUsers()
+    {
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+}
---
+++ ddsbe/app/Models/User.php
@@ -0,0 +1,10 @@
+App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class User extends Model{
+    protected $table = 'tbluser';
+    // column sa table
+    protected $fillable = [
+     'username', 'password'
+    ];
+}
